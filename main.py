42b966405f1b6c80b0d11ed14c9fe6c42913a702
Reed Essick <ressick@mit.edu>
2014-05-23 15:11:22 -0400

    added verbose statements and a few timing statements

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
+import time
+
 ###############################################################################
 ########################### specify parameters #############################
 
@@ -19,7 +21,8 @@ from optparse import OptionParser
 
 parser = OptionParser(usage=usage)
 
-parser.add_option("-v", "--verbose", dest="verbose", default=False, action="store_true")
+parser.add_option("-v", "--verbose", default=False, action="store_true")
+parser.add_option("", "--time", default=False, action="store_true", help="print statements about how long the code takes to run")
 
 parser.add_option("-n", "--nside-exp", default=7, type="int", help="HEALPix NSIDE parameter for pixelization is 2**opts.nside_exp")
 parser.add_option("-p", "--num-pol", default=2, type="int", help="the number of polarizations we attempt to reconstruct. should be either 1 or 2")
@@ -43,10 +46,10 @@ n_runs    = opts.num_runs
 freqs = np.arange(opts.fmin, opts.fmax, opts.df)
 
 ### load detectors
-if opts.verbose: print "loading list of detectors"
+if opts.verbose: 
+	print "loading list of detectors"
 if not args:
 	raise ValueError, "supply at least one detector name as an argument"
-
 detectors = {}
 for arg in args:
 	if arg == "H1": #"LHO":
@@ -65,53 +68,94 @@ nfreqs = len(freqs)
 ndet = len(detectors)
 print 'ndet = ', ndet
 
+if opts.verbose:
+	print "building prior objects"
+	if opts.time: to = time.time()
 hprior_amplitudes, hprior_means, hprior_covariance, num_gaus = priors.hpri_neg4(len_freqs=nfreqs, num_pol=num_pol)
-
-network = utils.Network(detectors=[detectors[i] for i in detectors], freqs=freqs, Np = num_pol)
 hprior = priors.hPrior(freqs=freqs, means=hprior_means, covariance=hprior_covariance, amplitudes=hprior_amplitudes, num_gaus=num_gaus, num_pol=num_pol)
 angprior = priors.angPrior(nside_exp=nside_exp)
+if opts.verbose and opts.time:
+	print "\t", time.time()-to
+
+if opts.verbose: 
+	print "building network"
+	if opts.time: to=time.time()
+network = utils.Network(detectors=detectors.values(), freqs=freqs, Np = num_pol)
+if opts.verbose:
+	print network
+	if opts.time: print "\t", time.time()-to
 
 ################################################################################
 ###################### init signal for each run ############################
 
+if opts.verbose:
+	print "generating %d random source locations"%n_runs
 cos_theta = np.random.uniform(low=-1., high=1., size=n_runs)
 phi = np.random.uniform(low=0., high=2.*np.pi, size=n_runs)
 angles = np.zeros((n_runs, 2.))
 angles[:,0] = np.arccos(cos_theta)
 angles[:,1] = phi
 
-for i_ang in xrange(n_runs):
-	
-	signal_h = inj.sinegaussian_f(f=freqs, to=0., phio=np.pi/2., fo=200., tau=0.01, hrss=5.e-22, alpha=np.pi/2)
+if opts.verobse:
+	print "computing waveform"
+signal_h = inj.sinegaussian_f(f=freqs, to=0., phio=np.pi/2., fo=200., tau=0.01, hrss=5.e-22, alpha=np.pi/2)
+if opts.verbose:
+	print "plotting waveform"
+	if opts.time: to=time.time()
+fig = plt.figure()
+plt.plot(freqs, np.real(signal_h[:,0]), 'r', label="Real h0")
+plt.plot(freqs, np.imag(signal_h[:,0]), 'ro', label="Imag h0")
+plt.plot(freqs, np.real(signal_h[:,1]), 'b', label="Real h1")
+plt.plot(freqs, np.imag(signal_h[:,1]), 'bo', label="Imag h1")
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Amplitude (for each polarization)")
+plt.legend(loc='best')
+plt.grid()
+fig.savefig("inj_hstrain.png")
+plt.close(fig)
+if opts.verbose and opts.time:
+	print "\t",time.time()-to
 
-	fig = plt.figure()
-	plt.plot(freqs, np.real(signal_h[:,0]), 'r', label="Real h0")
-	plt.plot(freqs, np.imag(signal_h[:,0]), 'ro', label="Imag h0")
-	plt.plot(freqs, np.real(signal_h[:,1]), 'b', label="Real h1")
-	plt.plot(freqs, np.imag(signal_h[:,1]), 'bo', label="Imag h1")
-	plt.xlabel("Frequency [Hz]")
-	plt.ylabel("Amplitude (for each polarization)")
-	plt.legend(loc='best')
-	plt.grid()
-	fig.savefig("inj_hstrain_nd%d_%d"%(ndet, i_ang))
-	plt.close(fig)
-	
+for i_ang in xrange(n_runs):
+	if opts.verbose:
+		print "injection %d / %d"%(i_ang+1,n_runs)
+
 	theta = angles[i_ang,0]
 	phi = angles[i_ang,1]
 
+	if opts.verbose:
+		print "computing antenna patterns"
+		if opts.time: to=time.time()
 	F = np.zeros((nfreqs, num_pol, ndet), 'complex')
+	if opts.verbose and opts.time:
+		print "\t", time.time()-to
+
+	if opts.verobse:
+		print "projecting waveform onto detectors"
+		if opts.time: to=time.time()
 	signal = np.zeros((nfreqs, ndet), 'complex')
 	for i_det, det in enumerate(network.detector_names_list()):
 		F[:,:,i_det] = network.F_det(det_name=det, theta=theta, phi=phi, psi=0.)
 		signal[:,i_det] = inj.project(F[:,:,i_det], signal_h)
-				
+	if opts.verbose and opts.time:
+		print "\t", time.time()-to
+
+	if opts.verbose:
+		print "generating noise"
+		if opts.time: to=time.time()
 	noise = np.zeros((nfreqs, ndet), 'complex')
 	for i_det, det in enumerate(network.detector_names_list()):
 		for i_f in xrange(nfreqs):
 			noise[i_f,i_det] = np.random.normal(loc=0, scale=np.sqrt( network.detectors[det].psd.interpolate(freqs[i_f]) ) ) * np.exp(np.random.uniform(0.,2*np.pi)*1.j)
-		
+	if opts.verbose and opts.time:
+		print "\t", time.time()-to
+
 	data = signal + noise
+
 	for i_det, det in enumerate(network.detector_names_list()):
+		if opts.verbose:
+			print "plotting data in detector:", det
+			if opts.time: to=time.time()
 		fig = plt.figure()
 		plt.plot(freqs, np.real(data[:,i_det]), 'r', label='Real')
 		plt.plot(freqs, np.imag(data[:,i_det]), 'b', label='Imag')
@@ -121,12 +165,19 @@ for i_ang in xrange(n_runs):
 		plt.grid()
 		fig.savefig("inj_data_%s_nd%d_%d"%(det, ndet, i_ang))
 		plt.close(fig)
+		if opts.verbose and opts.time:
+			print "\t", time.time()-to
 
 	###############################################################################
 	############################# Find posterior  ################################
 
+	if opts.verbose:
+		print "computing posterior"
+		if opts.time: to=time.time()
 	posterior = posteriors.Posterior(freqs=freqs, network=network, hprior=hprior, angprior=angprior, data=data, nside_exp=nside_exp)
 	post_built = posterior.build_posterior(num_proc=num_proc)
+	if opts.verbose and opts.time:
+		print "\t", time.time()-to
 
 	for i in xrange( np.shape(post_built)[0] ):
 		if post_built[i,1] == 0:
@@ -135,6 +186,9 @@ for i_ang in xrange(n_runs):
 	############################################################################
 	##########   Save Signal Stuff ############################################
 
+	if opts.verbose:
+		print "saving signal stuff"
+		if opts.time: to=time.time()
 	h_ML_theta_phi = posterior.h_ML(theta=theta, phi=phi)
 	save_strain0 = zip(signal_h[:,0], h_ML_theta_phi[:,0])
 	save_strain1 = zip(signal_h[:,1], h_ML_theta_phi[:,1])
@@ -145,9 +199,15 @@ for i_ang in xrange(n_runs):
 		save_data = zip(data[:,i_det], inj.project(F[:,:,i_det], h_ML_theta_phi))
 		np.savetxt('data_theta_phi_%s_nd%d_%d'%(det, ndet, i_ang), save_data)
 
+	if opts.verobse and opts.time:
+		print "\t", time.time()-to
+
 	############################################################################
 	####################   Deal with Posterior results ##########################
 
+	if opts.verbose:
+		print "saving posterior"
+		if opts.time: to=time.time()
 	np.savetxt('posterior_nd%d_%d'%(ndet, i_ang), post_built)
 
 	posterior.plot_posterior(posterior=post_built, figname='skymap_nd%d_%d'%(ndet, i_ang),title="theta %f, phi %f"%(theta,phi), unit='log posterior density', inj_theta=theta, inj_phi=phi)
@@ -158,4 +218,5 @@ for i_ang in xrange(n_runs):
 	fig.savefig("post_dens_hist_nd%d_%d"%(ndet, i_ang))
 	plt.close(fig)
 
-
+	if opts.verbose and opts.time:
+		print "\t", time.time()-to
