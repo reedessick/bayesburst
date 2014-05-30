451cfcc9c3310aea18210578818f4a41ee2ac4c6
Reed Essick <reed.essick@ligo.org>
2014-05-30 12:43:05 -0700

    added other metrics to stats computation

--- triangulation.py
+++ triangulation.py
@@ -314,35 +314,32 @@ if __name__ == "__main__":
 				statsfilename = "%s/stats-%d%s.txt"%(opts.output_dir, toa_ind, opts.tag)
 			
 				### angular offset between max of the posterior and injection
-#				cosDtheta = np.cos(est_theta)*np.cos(inj_theta) + np.sin(est_theta)*np.sin(inj_theta)*np.cos(est_phi - inj_phi)
 				cosDtheta = stats.cos_dtheta(est_theta, est_phi, inj_theta, inj_phi)
 
 				### searched area
 				injpix = hp.ang2pix(nside, inj_theta, inj_phi)
-#				posterior = posterior[posterior[:,1].argsort()[::-1]] # sort by posterior weight
-#				n_sapix = 0
-#				cum = 0.0
-#				for ipix, p in posterior:
-#					n_sapix += 1
-#					cum += p
-#					if ipix == injpix:
-#						break
-#				else:
-#					raise ValueError, "could not find injpix=%d in posterior"%injpix
-#				searched_area = pixarea_deg*n_sapix
 				cum = np.sum( posterior[:,1][posterior[:,1]>=posterior[injpix,1]] )
 				searched_area = stats.searched_area(posterior[:,1], inj_theta, inj_phi, degrees=True)
 
+				### num_mode
+				num_mode = stats.num_modes(posterior[:,1], inj_theta, inj_phi, nside=nside)
+
+				### min{cosDtheta}
+				min_cosDtheta = stats.min_cos_dtheta(posterior[:1], inj_theta, inj_phi, nside=nside)
+
 				### entropy
-				entropy = stats.entroyp(posterior[:,1])
+				entropy = stats.entropy(posterior[:,1])
+		
+				### information
+				info = stats.information(posterior[:,1])
 
 				statsfile = open(statsfilename, "w")
-				print >> statsfile, "cos(ang_offset) = %.6f\nsearched_area = %.6f deg2\np_value = %.6f\nentropy = %f"%(cosDtheta, searched_area, cum, entropy)
+				print >> statsfile, "cos(ang_offset) = %.6f\nsearched_area = %.6f deg2\np_value = %.6f\nnum_mode = %d\nmin{cos(ang_offset)} = %.6f\nentropy = %.6f\ninformation = %.6f"%(cosDtheta, searched_area, cum, num_mode, min_cosDtheta, entropy, info)
 				statsfile.close()
 
 				if opts.verbose: 
 					print "\t\t", statsfilename
-					print "\t\tcos(ang_offset) = %.6f\n\t\tsearched_area = %.6f deg2\np_value = %.6f\nentropy = %f"%(cosDtheta, searched_area, cum, entropy)
+					print "\t\tcos(ang_offset) = %.6f\n\t\tsearched_area = %.6f deg2\np_value = %.6f\nnum_mode = %d\nmin{cos(ang_offset)} = %.6f\nentropy = %.6f\ninformation = %.6f"%(cosDtheta, searched_area, cum, num_mode, min_cosDtheta, entropy, info)
 					if opts.time: print "\t\t", time.time()-to, "sec"
 
 		#================================================
