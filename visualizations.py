8458d5bcb2f4befa844b28f0374d75f347a865e9
Reed Essick <ressick@mit.edu>
2014-09-22 16:32:22 -0400

    added some thoughts and notes as comments

--- visualizations.py
+++ visualizations.py
@@ -3,3 +3,24 @@ usage="""a module that will house fancy visualization routines that interact wit
 #=================================================
 
 ### WRITE ME
+
+"""
+mollweide projections
+	different color-map options
+	inj, est position markers
+	cos_dtheta tracer
+	area shader (searched_area)
+	contours
+
+strain visualizations at a single position
+	mle estimate
+	mle +gaussian errors
+	MAP estimate? +error bars
+	at geocenter and in each detector datastream
+
+interactive mollweide with strain 
+
+
+else?
+"""
+	
