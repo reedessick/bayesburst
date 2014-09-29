917bcebf2b6f26bf04658cc4e88b5bb3a80aff02
Reed Essick <ressick@mit.edu>
2014-09-29 18:39:53 -0400

    added a reference dictionary of known detectors

--- detector_cache.py
+++ detector_cache.py
@@ -15,6 +15,8 @@ path = __file__.strip(__name__+".py")
 # DEFINE KNOWN DETECTORS
 #
 #=================================================
+detectors = {}
+
 default_psd = utils.PSD(np.array([0]), np.array([1]), kind="linear")
 
 ### design psd's
@@ -33,16 +35,19 @@ __H_nx__ = np.array((-0.2239, +0.7998, +0.5569))
 __H_ny__ = np.array((-0.9140, +0.0261, -0.4049))
 #LHO = Detector("LHO", __H_dr__, __H_nx__, __H_ny__, default_psd)
 LHO = utils.Detector("H1", __H_dr__, __H_nx__, __H_ny__, ligo_design_psd)
+detectors["H1"] = LHO
 
 __L_dr__ = np.array((-0.074276, -5.496284, +3.224257))*1e6/utils.c # sec
 __L_nx__ = np.array((-0.9546, -0.1416, -0.2622))
 __L_ny__ = np.array((+0.2977, -0.4879, -0.8205))
 #LLO = Detector("LLO", __L_dr__, __L_nx__, __L_ny__, default_psd)
 LLO = utils.Detector("L1", __L_dr__, __L_nx__, __L_ny__, ligo_design_psd)
+detectors["L1"] = LLO
 
 __V_dr__ = np.array((+4.546374, +0.842990, +4.378577))*1e6/utils.c # sec
 __V_nx__ = np.array((-0.7005, +0.2085, +0.6826))
 __V_ny__ = np.array((-0.0538, -0.9691, +0.2408))
 #Virgo = Detector("Virgo", __V_dr__, __V_nx__, __V_ny__, default_psd)
 Virgo = utils.Detector("V1", __V_dr__, __V_nx__, __V_ny__, virgo_design_psd)
+detectors["V1"] = Virgo
 
